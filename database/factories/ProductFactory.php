6692fe807b5e9a5a730c2edb1b3df7e546abc543
tawfiqAbir <tawfiq.abir@gmail.com>
2018-07-21 15:02:48 +0600

    faker value inserted to the table

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->name,
+        'detail'=>$faker->text,
+        'price'=>$faker->numberBetween(500,2500),
+        'stock'=>$faker->randomDigit,
+        'discount'=>$faker->numberBetween(2,30),
+
     ];
 });
