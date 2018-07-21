aab89b2b9af70cb61b07c8a3131e26abf5df3e92
tawfiqAbir <tawfiq.abir@gmail.com>
2018-07-21 15:13:15 +0600

    relationship builded

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Model;
+use App\Model\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        'name'=>$faker->name,
+        'name'=>$faker->word,
         'detail'=>$faker->text,
         'price'=>$faker->numberBetween(500,2500),
         'stock'=>$faker->randomDigit,
