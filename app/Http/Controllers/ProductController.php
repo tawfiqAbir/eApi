2bcbbb6ab1168efd506a48fe51ac51f4b37c2e2b
tawfiqAbir <tawfiq.abir@gmail.com>
2018-07-24 16:37:01 +0600

    delete product completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -104,6 +104,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null,204);
     }
 }
